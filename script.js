2a9bb5f0683bb278ec4668422b0a35e568cb0d8c
Anshu Kumar <117300557+anshu110125@users.noreply.github.com>
2023-05-22 12:28:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-// Your Script here.
+console.log("abc" + "kmz") // abckmz
 
 const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
+    'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q',
+    'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U',
+    'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y',
+    'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C',
+    'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G',
+    'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K',
+    'Y': 'L', 'Z': 'M', '?': '?', ',': ','
 };
-
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return ;//return decodedArr
+function encode(inputString){
+    let decoderArr = [];
+    for(let i = 0 ; i < inputString.length ; i++) {
+        if(lookup[ inputString[i] ] === undefined){
+            decoderArr.push( inputString[i] ) ;
+        }   
+        else{ 
+            decoderArr.push(lookup[ inputString[i] ]);
+        }
+    }
+    return decoderArr
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
+console.log(lookup["V"]);
+console.log(encode("#MNZD"));
